1e2057f807545fd9c02fdab09fded5eb8e208c54
Afonso247 <afonso.h2002@gmail.com>
2023-11-22 16:35:51 -0300

    Lógica de movimentação para todas as peças

--- src/components/GameComponent.vue
+++ src/components/GameComponent.vue
@@ -11,6 +11,9 @@
                 :data-type="tabs.type"
                 :data-color="tabs.color"
                 draggable="true"
+                @dragstart="handleDragStart"
+                @dragover="handleDragOver"
+                @drop="handleDrop"
                 v-html="tabs.svg"
             ></div>
         </div>
@@ -74,22 +77,73 @@ import tabs from '../data/data';
                 return;
             }
 
+            switch (draggedPiece.type) {
+                case 'pawn':
+                    if (this.validarMovimentoPeao(draggedPiece, droppedSquare)) {
+                        this.trocarPecas(draggedPiece, droppedSquare);
+                    } else {
+                        console.log('Movimento inválido para o peão');
+                    }
+                    break;
+                case 'bishop':
+                    if (this.validarMovimentoBispo(draggedPiece, droppedSquare)) {
+                        this.trocarPecas(draggedPiece, droppedSquare);
+                    } else {
+                        console.log('Movimento inválido para o bispo');
+                    }
+                    break;
+                case 'rook':
+                    if (this.validarMovimentoTorre(draggedPiece, droppedSquare)) {
+                        this.trocarPecas(draggedPiece, droppedSquare);
+                    } else {
+                        console.log('Movimento inválido para a torre');
+                    }
+                    break;
+                case 'knight':
+                    if (this.validarMovimentoCavalo(draggedPiece, droppedSquare)) {
+                        this.trocarPecas(draggedPiece, droppedSquare);
+                    } else {
+                        console.log('Movimento inválido para o cavalo');
+                    }
+                    break;
+                case 'queen':
+                    if (this.validarMovimentoRainha(draggedPiece, droppedSquare)) {
+                        this.trocarPecas(draggedPiece, droppedSquare);
+                    } else {
+                        console.log('Movimento inválido para a rainha');
+                    }
+                    break;
+                case 'king':
+                    if (this.validarMovimentoRei(draggedPiece, droppedSquare)) {
+                        this.trocarPecas(draggedPiece, droppedSquare);
+                    } else {
+                        console.log('Movimento inválido para o rei');
+                    }
+                    break;
+                default:
+                    console.log('movendo nada')
+                    return;
+            }
+
+            // this.trocarPecas(draggedPiece, droppedSquare);
+        },
+        trocarPecas(dragged, dropped) {
             // Vez das brancas
-            if(this.brancas && draggedPiece.color === 'white') {
-                if(droppedSquare.color !== 'white') {
+            if(this.brancas && dragged.color === 'white') {
+                if(dropped.color !== 'white') {
                     // Trocar os atributos "id" entre os objetos
-                    const idTemp = this.tabuleiro[draggedPiece.id].id;
-                    this.tabuleiro[draggedPiece.id].id = this.tabuleiro[droppedSquare.id].id;
-                    this.tabuleiro[droppedSquare.id].id = idTemp;
+                    const idTemp = this.tabuleiro[dragged.id].id;
+                    this.tabuleiro[dragged.id].id = this.tabuleiro[dropped.id].id;
+                    this.tabuleiro[dropped.id].id = idTemp;
 
                     // Trocar os objetos de posição na this.tabuleiro
-                    if(droppedSquare.color === 'black') {
-                        this.tabuleiro[draggedPiece.id] = this.tabuleiro[droppedSquare.id];
-                        this.tabuleiro[droppedSquare.id] = { id: idTemp, type: 'empty', svg: ''};
+                    if(dropped.color === 'black') {
+                        this.tabuleiro[dragged.id] = this.tabuleiro[dropped.id];
+                        this.tabuleiro[dropped.id] = { id: idTemp, type: 'empty', svg: ''};
                     } else {
-                        const temp = this.tabuleiro[draggedPiece.id];
-                        this.tabuleiro[draggedPiece.id] = this.tabuleiro[droppedSquare.id];
-                        this.tabuleiro[droppedSquare.id] = temp;
+                        const temp = this.tabuleiro[dragged.id];
+                        this.tabuleiro[dragged.id] = this.tabuleiro[dropped.id];
+                        this.tabuleiro[dropped.id] = temp;
                     }
 
                     this.passarVez()
@@ -99,22 +153,22 @@ import tabs from '../data/data';
                 }
             
             // Vez das negras
-            } else if (this.negras && draggedPiece.color === 'black') {
-                if(droppedSquare.color !== 'black') {
+            } else if (this.negras && dragged.color === 'black') {
+                if(dropped.color !== 'black') {
                     // Trocar os atributos "id" entre os objetos
-                    const idTemp = this.tabuleiro[draggedPiece.id].id;
-                    this.tabuleiro[draggedPiece.id].id = this.tabuleiro[droppedSquare.id].id;
-                    this.tabuleiro[droppedSquare.id].id = idTemp;
+                    const idTemp = this.tabuleiro[dragged.id].id;
+                    this.tabuleiro[dragged.id].id = this.tabuleiro[dropped.id].id;
+                    this.tabuleiro[dropped.id].id = idTemp;
 
                     // Trocar os objetos de posição na this.tabuleiro
                     // Trocar os objetos de posição na this.tabuleiro
-                    if(droppedSquare.color === 'white') {
-                        this.tabuleiro[draggedPiece.id] = this.tabuleiro[droppedSquare.id];
-                        this.tabuleiro[droppedSquare.id] = { id: idTemp, type: 'empty', svg: ''};
+                    if(dropped.color === 'white') {
+                        this.tabuleiro[dragged.id] = this.tabuleiro[dropped.id];
+                        this.tabuleiro[dropped.id] = { id: idTemp, type: 'empty', svg: ''};
                     } else {
-                        const temp = this.tabuleiro[draggedPiece.id];
-                        this.tabuleiro[draggedPiece.id] = this.tabuleiro[droppedSquare.id];
-                        this.tabuleiro[droppedSquare.id] = temp;
+                        const temp = this.tabuleiro[dragged.id];
+                        this.tabuleiro[dragged.id] = this.tabuleiro[dropped.id];
+                        this.tabuleiro[dropped.id] = temp;
                     }
 
                     this.passarVez()
@@ -123,6 +177,164 @@ import tabs from '../data/data';
                     return;
                 }
             }
+        },
+        validarMovimentoPeao(dragged, dropped) {
+            const distance = Math.abs(dragged.id - dropped.id);
+            const direction = dragged.color === 'white' ? -1 : 1;
+
+            // Movimento de duas casas para frente no primeiro movimento
+            if (distance === 16 && dropped.type === 'empty') {
+                // Verifica se é o primeiro movimento do peão
+                if ((dragged.color === 'white' && dragged.id > 47) || (dragged.color === 'black' && dragged.id < 16)) {
+                    // Verifica se não há peças no caminho
+                    const middleSquareId = dragged.id + 8 * direction;
+                    console.log(middleSquareId);
+                    const middleSquare = this.tabuleiro.find(square => square.id === middleSquareId);
+                    if (middleSquare.type === 'empty') {
+                        return true;
+                    }
+                }
+            }
+
+            // Movimento de uma casa para frente
+            if (distance === 8 && dropped.type === 'empty') {
+                return true;
+            }
+
+            // Captura na diagonal
+            if (distance === 9 || distance === 7) {
+                const diagonalPiece = this.tabuleiro.find(piece => piece.id === dropped.id);
+                if(diagonalPiece.type === 'empty') {
+                    return false;
+                }
+                return diagonalPiece && diagonalPiece.color !== dragged.color;
+            }
+
+            return false;
+        },
+        validarMovimentoBispo(dragged, dropped) {
+            const distanceRow = Math.abs(Math.floor(dragged.id / 8) - Math.floor(dropped.id / 8));
+            const distanceCol = Math.abs((dragged.id % 8) - (dropped.id % 8));
+
+            // Verificar se o movimento é diagonal (mesma distância nas linhas e colunas)
+            if (distanceRow === distanceCol) {
+                const rowDirection = dragged.id < dropped.id ? 1 : -1;
+                const colDirection = dragged.id % 8 < dropped.id % 8 ? 1 : -1;
+
+                // Verificar se não há peças no caminho do bispo
+                for (let i = 1; i < distanceRow; i++) {
+                    const intermediateSquareId = dragged.id + i * 8 * rowDirection + i * colDirection;
+                    const intermediateSquare = this.tabuleiro.find(square => square.id === intermediateSquareId);
+                    if (intermediateSquare.type !== 'empty') {
+                        return false; // Existe uma peça bloqueando o caminho
+                    }
+                }
+
+                const destinationSquare = this.tabuleiro.find(square => square.id === dropped.id);
+                return destinationSquare.type === 'empty' || destinationSquare.color !== dragged.color;
+            }
+
+            return false; // Movimento inválido para o bispo
+        },
+        validarMovimentoTorre(dragged, dropped) {
+            const distanceRow = Math.abs(Math.floor(dragged.id / 8) - Math.floor(dropped.id / 8));
+            const distanceCol = Math.abs((dragged.id % 8) - (dropped.id % 8));
+
+            // Verificar se o movimento é na mesma linha ou coluna
+            if ((distanceRow === 0 && distanceCol !== 0) || (distanceCol === 0 && distanceRow !== 0)) {
+                const rowDirection = dragged.id < dropped.id ? 1 : -1;
+                const colDirection = dragged.id % 8 < dropped.id % 8 ? 1 : -1;
+
+                // Verificar se não há peças no caminho da torre
+                if (distanceRow > 0) {
+                    for (let i = 1; i < distanceRow; i++) {
+                        const intermediateSquareId = dragged.id + i * 8 * rowDirection;
+                        const intermediateSquare = this.tabuleiro.find(square => square.id === intermediateSquareId);
+                        if (intermediateSquare.type !== 'empty') {
+                            return false; // Existe uma peça bloqueando o caminho
+                        }
+                    }
+                } else {
+                    for (let i = 1; i < distanceCol; i++) {
+                        const intermediateSquareId = dragged.id + i * colDirection;
+                        const intermediateSquare = this.tabuleiro.find(square => square.id === intermediateSquareId);
+                        if (intermediateSquare.type !== 'empty') {
+                            return false; // Existe uma peça bloqueando o caminho
+                        }
+                    }
+                }
+
+                const destinationSquare = this.tabuleiro.find(square => square.id === dropped.id);
+                return destinationSquare.type === 'empty' || destinationSquare.color !== dragged.color;
+            }
+
+            return false; // Movimento inválido para a torre
+        },
+        validarMovimentoCavalo(dragged, dropped) {
+            const distanceRow = Math.abs(Math.floor(dragged.id / 8) - Math.floor(dropped.id / 8));
+            const distanceCol = Math.abs((dragged.id % 8) - (dropped.id % 8));
+
+            // Verificar se o movimento é em forma de "L" (2 espaços em uma direção e 1 espaço perpendicular)
+            if ((distanceRow === 2 && distanceCol === 1) || (distanceRow === 1 && distanceCol === 2)) {
+                const destinationSquare = this.tabuleiro.find(square => square.id === dropped.id);
+                return destinationSquare.type === 'empty' || destinationSquare.color !== dragged.color;
+            }
+
+            return false; // Movimento inválido para o cavalo
+        },
+        validarMovimentoRainha(dragged, dropped) {
+            const distanceRow = Math.abs(Math.floor(dragged.id / 8) - Math.floor(dropped.id / 8));
+            const distanceCol = Math.abs((dragged.id % 8) - (dropped.id % 8));
+
+            // Verificar se o movimento é na mesma linha, coluna ou diagonal
+            if ((distanceRow === 0 && distanceCol !== 0) || (distanceCol === 0 && distanceRow !== 0) || (distanceRow === distanceCol)) {
+                const rowDirection = dragged.id < dropped.id ? 1 : -1;
+                const colDirection = dragged.id % 8 < dropped.id % 8 ? 1 : -1;
+
+                // Verificar se não há peças no caminho
+                if (distanceRow > 0 && distanceCol === 0) { // Movimento na mesma coluna
+                    for (let i = 1; i < distanceRow; i++) {
+                        const intermediateSquareId = dragged.id + i * 8 * rowDirection;
+                        const intermediateSquare = this.tabuleiro.find(square => square.id === intermediateSquareId);
+                        if (intermediateSquare.type !== 'empty') {
+                            return false; // Existe uma peça bloqueando o caminho
+                        }
+                    }
+                } else if (distanceCol > 0 && distanceRow === 0) { // Movimento na mesma linha
+                    for (let i = 1; i < distanceCol; i++) {
+                        const intermediateSquareId = dragged.id + i * colDirection;
+                        const intermediateSquare = this.tabuleiro.find(square => square.id === intermediateSquareId);
+                        if (intermediateSquare.type !== 'empty') {
+                            return false; // Existe uma peça bloqueando o caminho
+                        }
+                    }
+                } else if (distanceRow === distanceCol) { // Movimento diagonal
+                    for (let i = 1; i < distanceRow; i++) {
+                        const intermediateSquareId = dragged.id + i * 8 * rowDirection + i * colDirection;
+                        const intermediateSquare = this.tabuleiro.find(square => square.id === intermediateSquareId);
+                        if (intermediateSquare.type !== 'empty') {
+                            return false; // Existe uma peça bloqueando o caminho
+                        }
+                    }
+                }
+
+                const destinationSquare = this.tabuleiro.find(square => square.id === dropped.id);
+                return destinationSquare.type === 'empty' || destinationSquare.color !== dragged.color;
+            }
+
+            return false; // Movimento inválido para a rainha
+        },
+        validarMovimentoRei(dragged, dropped) {
+            const distanceRow = Math.abs(Math.floor(dragged.id / 8) - Math.floor(dropped.id / 8));
+            const distanceCol = Math.abs((dragged.id % 8) - (dropped.id % 8));
+
+            // Verificar se o movimento é de apenas uma casa em qualquer direção
+            if ((distanceRow === 1 && distanceCol === 0) || (distanceRow === 0 && distanceCol === 1) || (distanceRow === 1 && distanceCol === 1)) {
+                const destinationSquare = this.tabuleiro.find(square => square.id === dropped.id);
+                return destinationSquare.type === 'empty' || destinationSquare.color !== dragged.color;
+            }
+
+            return false; // Movimento inválido para o rei
         },
         isBeige(index) {
             const row = Math.floor(index / this.width);
@@ -135,13 +347,6 @@ import tabs from '../data/data';
     },
     mounted() {
         this.iniciarPartida();
-        const squares = document.querySelectorAll('.quadrado');
-
-        squares.forEach(square => {
-            square.addEventListener('dragstart', this.handleDragStart);
-            square.addEventListener('dragover', this.handleDragOver);
-            square.addEventListener('drop', this.handleDrop);
-        });
     }
 }
 </script>
